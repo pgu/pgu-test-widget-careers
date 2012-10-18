d0e40b5ad79f94d8c1700c547aa347880a0b462c
Pascal Guilcher <guilcher.p@sfeir.com>
2012-10-18 13:43:07 +0200

    ..

---
+++ src/pgu/widget/careers/server/MenuServlet.java
@@ -0,0 +1,42 @@
+package pgu.widget.careers.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class MenuServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print(getMenu());
+    }
+
+    public static String getMenu() {
+        final String menuJson = "" + //
+                "{'entries':                                " + //
+                "           [                               " + //
+                "            {                              " + //
+                "              'code':'0'                   " + //
+                "             ,'title':'Careers'            " + //
+                "            }                              " + //
+                "           ]                               " + //
+                "}                                          " + //
+                ""; //
+
+        return menuJson //
+                .replaceAll("'", "\"") //
+                .replaceAll("\\s", "") //
+                ;
+    }
+    //
+    public static void main(final String[] args) {
+        System.out.println(MenuServlet.getMenu());
+
+        System.out.println("1.to".contains("."));
+    }
+
+}
